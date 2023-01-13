a1a2a199b89e04cbfc2f3dff14515b895968edf0
RnhT01 <68704607+RnhT01@users.noreply.github.com>
2023-01-13 14:32:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/project', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    const value =  {email: [
+      { type: 'required', message: "L'email est requis" },
+      { type: 'pattern', message: 'Entrer un email valide' },
+    ],}
+    res.json(value)
 })
 app.listen(process.env.PORT || 3000)
